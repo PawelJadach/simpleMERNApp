0111173412dcac56500cc6ad7b89a6d356b852cf
unknown <pawel1234477@wp.pl>
2019-06-13 15:17:36 +0200

    Last commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,18 +17,18 @@ router.post('/', (req, res) => {
 
   // Simple validation
   if(!email || !password) {
-    return res.status(400).json({ msg: 'Please enter all fields' });
+    return res.status(400).json({ msg: 'Uzupełnij wszystkie pola' });
   }
 
   // Check for existing user
   User.findOne({ email })
     .then(user => {
-      if(!user) return res.status(400).json({ msg: 'User Does not exist' });
+      if(!user) return res.status(400).json({ msg: 'Użytkownik nie znaleziony' });
 
       // Validate password
       bcrypt.compare(password, user.password)
         .then(isMatch => {
-          if(!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+          if(!isMatch) return res.status(400).json({ msg: 'Nieprawidłowe dane' });
 
           jwt.sign(
             { id: user.id },
